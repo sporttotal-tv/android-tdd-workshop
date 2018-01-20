b01bc58cb858a8340cfd763f188669c018325d5c
Danny Preussler <dpreussler@web.de>
2018-01-20 16:57:38 +0100

    skeleton

---
+++ app/src/test/java/de/jodamob/mytddapplication/FirstTest.kt
@@ -0,0 +1,12 @@
+package de.jodamob.mytddapplication
+
+import junit.framework.Assert.fail
+import org.junit.Test
+
+class FirstTest {
+
+    @Test
+    fun test() {
+        fail("TO DO")
+    }
+}
