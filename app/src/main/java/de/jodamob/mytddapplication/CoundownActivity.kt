f875ad6fd23365eb53fa1c2f8965129fb520b7ed
Danny Preussler <dpreussler@web.de>
2018-02-09 09:25:32 +0100

    syntethtics

--- app/src/main/java/de/jodamob/mytddapplication/CoundownActivity.kt
+++ app/src/main/java/de/jodamob/mytddapplication/CoundownActivity.kt
@@ -3,33 +3,30 @@ package de.jodamob.mytddapplication
 import android.app.Activity
 import android.databinding.Observable
 import android.databinding.ObservableField
-import android.databinding.ObservableInt
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_countdown.*
 import toothpick.Toothpick
 import javax.inject.Inject
+
 
 open class CoundownActivity : Activity() {
 
     @Inject lateinit var viewmodel: CountdownViewModel
-    val counterText : TextView by lazy { findViewById<TextView>(R.id.time_value) }
-    val startButton : Button by lazy { findViewById<Button>(R.id.start_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_countdown)
 
         injectFields()
-        counterText.text = viewmodel.counterString.get()
-        startButton.setOnClickListener { viewmodel.start() }
+        time_value.text = viewmodel.counterString.get()
+        start_button.setOnClickListener { viewmodel.start() }
         viewmodel.counterString.observe()
     }
 
     private fun ObservableField<String>.observe() {
         addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
                 override fun onPropertyChanged(p0: Observable?, p1: Int) {
-                    counterText.setText((p0 as ObservableField<String>).get())
+                    time_value.setText((p0 as ObservableField<String>).get())
                 }
         })
     }
